49f080a2ad1acaeb4471f33aff4d8afc561edb2b
kymx1983 <kymx1983@gmail.com>
2021-01-23 21:45:23 +0900

    Userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_121144) do
+ActiveRecord::Schema.define(version: 2021_01_23_124350) do
 
   create_table "issues", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_01_23_121144) do
     t.integer "user_id"
     t.date "limit_date"
     t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login_id"
+    t.string "password"
+    t.string "user_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
