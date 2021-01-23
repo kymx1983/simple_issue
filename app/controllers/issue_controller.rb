d318bfe8168a07bf71b16f21c9291ddc2c52fc85
kymx1983 <kymx1983@gmail.com>
2021-01-23 22:01:07 +0900

    issue/create作成

--- app/controllers/issue_controller.rb
+++ app/controllers/issue_controller.rb
@@ -8,4 +8,22 @@ class IssueController < ApplicationController
   def new
     @issue = Issue.new
   end
+
+  def create
+
+    @issue = Issue.new
+    @issue.title = params[:title]
+    @issue.content = params[:content]
+    @issue.limit_date = params[:limit_date]
+    @issue.status = 0
+    # @issue.user_id = @current_user.id
+
+    if @issue.save
+      flash[:notice] = "「#{@issue.title}」を追加しました。"
+      redirect_to('/issue/index')
+    else
+      flash[:alert] = "入力内容に誤りがあります。ご確認ください。"
+      render("issue/new")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'issue/index'
   get 'issue/show'
   get 'issue/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "issue/create"
 end
